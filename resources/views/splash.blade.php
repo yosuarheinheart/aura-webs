ae71f1cf44ec9bac18f00110693366211b12be5c
yosuarheinheart <yosuatanzil10@gmail.com>
2025-08-04 11:17:25 +0700

    Include icon aura

--- resources/views/splash.blade.php
+++ resources/views/splash.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="icon" href="{{ asset('asset/logoaura2025.png') }}">
     <title>AURA UMN - Welcome</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700;800&display=swap');
